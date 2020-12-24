facf6edfaee1646da959dd9d365c3a938412b36b
Vault108 <44564111+Vault108@users.noreply.github.com>
2020-12-24 17:14:10 -0500

    fixes redundant code
    Signed-off-by: Vault108 <44564111+Vault108@users.noreply.github.com>

--- Vaults-Scripts/python/insult.py
+++ Vaults-Scripts/python/insult.py
@@ -4,7 +4,6 @@ r = requests.get("https://evilinsult.com/generate_insult.php?lang=en&type=json")
 r.headers["content-type"]
 r.encoding
 r.text
-r.json()
 json = r.json()
 response = json["insult"]
 print(response)
